5d44cf4d7346393fec923d4e77278066889a5a0e
Alex Khramov <khramulka@ya.ru>
2025-04-29 12:20:03 +0000

    Rebase update

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,9 +1,15 @@
+
+
 #!/bin/bash
 # display command line options
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> 854da09 (git-rebase 1)
     count=$(( $count + 1 ))
 done
 
